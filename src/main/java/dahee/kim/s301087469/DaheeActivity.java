8df63fb6419c9039af8c4d6656baa99876641256
hailey0882 <hailey0882@gmail.com>
2020-10-31 08:54:02 -0400

    Initial commit

---
+++ src/main/java/dahee/kim/s301087469/DaheeActivity.java
@@ -0,0 +1,15 @@
+/* Dahee Kim, 301087469, Section02 */
+package dahee.kim.s301087469;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaheeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dahee);
+    }
+}
